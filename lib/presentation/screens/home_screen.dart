163dd0e6ac9a8dd6193d5af83db501b65a7d7375
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2022-08-16 01:20:57 +0800

    debugsy

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -19,132 +19,143 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BlocBuilder<InternetCubit, InternetState>(
-                builder: (context, state) {
-              if (state is InternetConnected &&
-                  state.connectionType == ConnectionType.Wifi) {
-                return Text(
-                  'Wi-fi',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline3!
-                      .copyWith(color: Colors.green),
-                );
-              } else if (state is InternetConnected &&
-                  state.connectionType == ConnectionType.Mobile) {
-                return Text(
-                  'Mobile',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline3!
-                      .copyWith(color: Colors.blue),
-                );
-              } else if (state is InternetDisconnected) {
-                return Text(
-                  'Disconnected',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline3!
-                      .copyWith(color: Colors.red),
-                );
-              }
-              return CircularProgressIndicator();
-            }),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            const Text(
-              'This is where the counter value should be displayed',
-            ),
-            BlocBuilder<CounterCubit, CubitState>(
-              builder: (context, state) {
-                if (state.counterValue < 0) {
+    return BlocListener<InternetCubit, InternetState>(
+      listener: (context, state) {
+        if (state is InternetConnected &&
+            state.connectionType == ConnectionType.Wifi) {
+          context.read<CounterCubit>().increment();
+        } else if (state is InternetConnected &&
+            state.connectionType == ConnectionType.Mobile) {
+          context.read<CounterCubit>().decrement();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              BlocBuilder<InternetCubit, InternetState>(
+                  builder: (context, state) {
+                if (state is InternetConnected &&
+                    state.connectionType == ConnectionType.Wifi) {
                   return Text(
-                    'BRR,NEGATIVE' + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
+                    'Wi-fi',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3!
+                        .copyWith(color: Colors.green),
                   );
-                } else if (state.counterValue % 2 == 0) {
+                } else if (state is InternetConnected &&
+                    state.connectionType == ConnectionType.Mobile) {
                   return Text(
-                    'YAYYY' + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
+                    'Mobile',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3!
+                        .copyWith(color: Colors.blue),
                   );
-                } else if (state.counterValue == 5) {
+                } else if (state is InternetDisconnected) {
                   return Text(
-                    'HMMMM' + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else {
-                  return Text(
-                    state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
+                    'Disconnected',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3!
+                        .copyWith(color: Colors.red),
                   );
                 }
-              },
-            ),
-            // Text(
-            //   '$_counter',
-            // ),
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //   children: [
-            //     FloatingActionButton(
-            //       heroTag: Text('${widget.title}#1'),
-            //       onPressed: () {
-            //         BlocProvider.of<CubitIncrementDecrement>(context)
-            //             .decrement();
-            //       },
-            //       tooltip: 'Decrement',
-            //       child: Icon(Icons.remove),
-            //     ),
-            //     FloatingActionButton(
-            //       heroTag: Text('${widget.title}#2'),
-            //       onPressed: () {
-            //         BlocProvider.of<CubitIncrementDecrement>(context)
-            //             .increment();
-            //         // context.bloc<CubitsampleCubit>().increment();
-            //       },
-            //       tooltip: 'Increment',
-            //       child: Icon(Icons.add_outlined),
-            //     )
-            //   ],
-            // ),
-            const SizedBox(
-              height: 24,
-            ),
-            Builder(
-              builder: (materialButtonContext) => MaterialButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/second');
-                },
-                child: Text('Go to Second Screen'),
-                color: widget.colors,
+                return CircularProgressIndicator();
+              }),
+              const Text(
+                'You have pushed the button this many times:',
               ),
-            ),
-            Builder(
-              builder: (materialButtonContext) => MaterialButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/third');
-                },
-                child: Text('Go to Third Screen'),
-                color: widget.colors,
+              const Text(
+                'This is where the counter value should be displayed',
               ),
-            ),
-          ],
+              BlocBuilder<CounterCubit, CubitState>(
+                builder: (context, state) {
+                  if (state.counterValue < 0) {
+                    return Text(
+                      'BRR,NEGATIVE' + state.counterValue.toString(),
+                      style: Theme.of(context).textTheme.headline4,
+                    );
+                  } else if (state.counterValue % 2 == 0) {
+                    return Text(
+                      'YAYYY' + state.counterValue.toString(),
+                      style: Theme.of(context).textTheme.headline4,
+                    );
+                  } else if (state.counterValue == 5) {
+                    return Text(
+                      'HMMMM' + state.counterValue.toString(),
+                      style: Theme.of(context).textTheme.headline4,
+                    );
+                  } else {
+                    return Text(
+                      state.counterValue.toString(),
+                      style: Theme.of(context).textTheme.headline4,
+                    );
+                  }
+                },
+              ),
+              // Text(
+              //   '$_counter',
+              // ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //   children: [
+              //     FloatingActionButton(
+              //       heroTag: Text('${widget.title}#1'),
+              //       onPressed: () {
+              //         BlocProvider.of<CubitIncrementDecrement>(context)
+              //             .decrement();
+              //       },
+              //       tooltip: 'Decrement',
+              //       child: Icon(Icons.remove),
+              //     ),
+              //     FloatingActionButton(
+              //       heroTag: Text('${widget.title}#2'),
+              //       onPressed: () {
+              //         BlocProvider.of<CubitIncrementDecrement>(context)
+              //             .increment();
+              //         // context.bloc<CubitsampleCubit>().increment();
+              //       },
+              //       tooltip: 'Increment',
+              //       child: Icon(Icons.add_outlined),
+              //     )
+              //   ],
+              // ),
+              const SizedBox(
+                height: 24,
+              ),
+              Builder(
+                builder: (materialButtonContext) => MaterialButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/second');
+                  },
+                  child: Text('Go to Second Screen'),
+                  color: widget.colors,
+                ),
+              ),
+              Builder(
+                builder: (materialButtonContext) => MaterialButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/third');
+                  },
+                  child: Text('Go to Third Screen'),
+                  color: widget.colors,
+                ),
+              ),
+            ],
+          ),
         ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: _incrementCounter,
+        //   tooltip: 'Increment',
+        //   child: const Icon(Icons.add),
+        // ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
